30d8ecfb7263529b71616c9cccc44a32e102ade9
saif177 <saif.blt17@gmail.com>
2021-02-15 14:12:10 +0530

    master
    expense tracking system using php

---
+++ tblexpense.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2019 at 04:42 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `detsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblexpense`
+--
+
+CREATE TABLE `tblexpense` (
+  `ID` int(10) NOT NULL,
+  `UserId` int(10) NOT NULL,
+  `ExpenseDate` date DEFAULT NULL,
+  `ExpenseItem` varchar(200) DEFAULT NULL,
+  `ExpenseCost` varchar(200) DEFAULT NULL,
+  `NoteDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblexpense`
+--
+
+INSERT INTO `tblexpense` (`ID`, `UserId`, `ExpenseDate`, `ExpenseItem`, `ExpenseCost`, `NoteDate`) VALUES
+(1, 2, '2019-05-15', 'Milk', '63', NULL),
+(2, 2, '2019-05-15', 'Vegitables', '520', '2019-05-15 10:06:19'),
+(3, 2, '2019-05-15', 'Household Items', '5200', '2019-05-15 10:07:08'),
+(4, 2, '2019-05-14', 'Milk', '83', '2019-05-15 10:07:27'),
+(5, 2, '2019-05-14', 'Bed Sheets', '1120', '2019-05-15 10:07:49'),
+(6, 2, '2019-05-12', 'Fruits', '890', '2019-05-15 10:08:09'),
+(7, 2, '2019-05-10', 'Household Items', '5600', '2019-05-15 10:08:26'),
+(8, 2, '2019-04-24', 'Milk', '102', '2019-05-15 10:08:44'),
+(9, 2, '2019-05-08', 'Bed Sheets', '890', '2019-05-15 10:08:57'),
+(10, 2, '2018-12-19', 'Household Items', '1120', '2019-05-15 10:09:34'),
+(11, 2, '2018-12-19', 'Fruits', '560', '2019-05-15 10:09:52'),
+(13, 2, '2018-12-20', 'Tour of Manali', '30000', '2019-05-15 10:15:47'),
+(14, 2, '2019-05-14', 'Milk', '360', '2019-05-15 10:21:31'),
+(15, 3, '2019-05-15', 'Milk', '123', '2019-05-15 10:29:56'),
+(16, 3, '2019-05-15', 'Household Items', '360', '2019-05-15 10:30:06'),
+(17, 3, '2019-05-15', 'Bed Sheets', '3000', '2019-05-15 10:30:18'),
+(18, 3, '2019-05-07', 'Milk', '123', '2019-05-15 10:30:28'),
+(19, 3, '2019-05-14', 'Household Items', '3600', '2019-05-15 10:30:38'),
+(20, 2, '2019-05-14', 'Electric Board Extension', '300', '2019-05-15 15:11:33'),
+(21, 2, '2019-04-11', 'Milk', '123', '2019-05-15 17:50:24'),
+(22, 2, '2019-04-10', 'Household Items', '520', '2019-05-15 17:50:37'),
+(23, 2, '2019-05-16', 'Household Items', '360', '2019-05-16 07:29:54'),
+(25, 8, '2019-05-17', 'Milk', '3600', '2019-05-17 05:35:13'),
+(26, 8, '2019-05-16', 'Bed Sheets', '1025', '2019-05-17 05:35:42'),
+(27, 1, '2019-05-17', 'Computer Mouse', '500', '2019-05-18 05:19:05'),
+(30, 1, '2019-05-18', 'Milk + Bread', '80', '2019-05-18 05:22:01'),
+(31, 10, '2019-05-16', 'Computer Mouse', '500', '2019-05-18 05:35:45'),
+(32, 10, '2019-05-17', 'Milk+Bread', '80', '2019-05-18 05:36:06'),
+(33, 10, '2019-05-18', 'Room Rent', '10000', '2019-05-18 05:36:26'),
+(35, 11, '2019-11-14', 'eggs', '60', '2019-11-14 03:52:37'),
+(36, 11, '2019-11-13', 'peanut butter', '520', '2019-11-14 03:53:11'),
+(37, 11, '2019-11-02', 'rent', '8500', '2019-11-14 03:53:37'),
+(38, 11, '2019-11-05', 'petrol', '400', '2019-11-14 03:54:22'),
+(39, 11, '2019-10-05', 't-shirt', '1200', '2019-11-14 03:54:48'),
+(40, 11, '2019-10-11', 'headphone', '8500', '2019-11-14 03:56:20'),
+(42, 11, '2019-11-16', 'bread', '40', '2019-11-16 03:35:10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblexpense`
+--
+ALTER TABLE `tblexpense`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblexpense`
+--
+ALTER TABLE `tblexpense`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
